4c6b3651a72b4a3bff7bb0b961eb26c61dbf0c7c
Oleksandr Hudzovatyi <125153526+olekshudz@users.noreply.github.com>
2024-03-01 11:06:01 -0500

    update project structure and add tpx-5 new proposal for a customer (#1)

---
+++ src/test/java/pages/proposals/ProposalsPage.java
@@ -0,0 +1,28 @@
+package pages.proposals;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utils.BrowserUtils;
+
+public class ProposalsPage {
+
+    public ProposalsPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(partialLinkText = "New Proposal")
+    private WebElement newProposalButton;
+
+    @FindBy(xpath = "//span[contains(text(), 'Sent')]")
+    private WebElement status;
+
+    public void clickNewProposal() {
+        this.newProposalButton.click();
+    }
+
+    public String getStatus() {
+        return BrowserUtils.getText(status);
+    }
+}
