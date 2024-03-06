05bedf0606b62715693be60f716efa2cdce5e5f0
meanpeach <alisterag@gmail.com>
2024-03-06 16:37:52 -0500

    Revert "Tpx 6 verify created proposal as a customer (#6)"
    This reverts commit 9458dd93b954444242b89257d0411556be8c03f7.

--- src/test/java/utils/DriverHelper.java
+++ src/test/java/utils/DriverHelper.java
@@ -2,13 +2,12 @@ package utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
-
 import java.net.MalformedURLException;
 import java.net.URL;
-
 import java.time.Duration;
 
 public class DriverHelper {
